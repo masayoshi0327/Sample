4a29e40a231e3bdc1a4d1be856cfd833a3287526
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-02-13 16:41:27 +0900

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by 澤崎正佳 on 2021/02/13.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
